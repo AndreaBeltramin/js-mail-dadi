626cb7f2b6595a10f406ac48409fc44a0111ed98
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-17 17:03:57 +0200

    secondo es

--- ex 2/js/main.js
+++ ex 2/js/main.js
@@ -2,13 +2,30 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-//Che ci sia un array da qualche parte?
-//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+//!RACCOLTA DATI
 
-//Consigli del giorno:
-//1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-//2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-//3. si ma noi cosa vogliamo fare?
-//4. torniamo a scrivere in italiano
-//5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+let i = 0;
+let min = 1;
+let max = 6;
+
+//genero un numero casuale da 1 a 6 per l'utente
+let casualNumberUser = Math.floor(Math.random() * 6) + 1;
+console.log("Il tuo numero è " + casualNumberUser);
+
+//genero un numero casuale da 1 a 6 per il computer
+let casualNumberComputer = Math.floor(Math.random() * 6) + 1;
+console.log("Il numero del computer è " + casualNumberComputer);
+
+//!ELABORAZIONE
+//stabilisco quale dei 2 numeri è maggiore e quindi chi vince
+
+if (casualNumberUser > casualNumberComputer) {
+	console.log("Complimenti, hai vinto!!");
+	alert("Complimenti, hai vinto!!");
+} else if (casualNumberUser < casualNumberComputer) {
+	console.log("Mi dispiace, hai perso!!");
+	alert("Mi dispiace, hai perso!!");
+} else if (casualNumberUser === casualNumberComputer) {
+	console.log("Pareggio!!");
+	alert("Pareggio!!");
+}
